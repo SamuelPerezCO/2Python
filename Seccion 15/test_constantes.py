bc03fb89603211793f80a3d04ff843fe8d4f2486
SamuelPerezCO <sernasamuelperez@gmail.com>
2024-02-12 16:20:07 -0500

    Seccion 15

---
+++ Seccion 15/test_constantes.py
@@ -0,0 +1,9 @@
+from constantes import *
+from constantes import Matematicas as Mate
+
+print(MI_CONSTANTE)
+print(Mate.PI)
+
+#No debemos cambiar el valor
+#MI_CONSTANTE = 'Nuevo Valor'
+#print(MI_CONSTANTE)
