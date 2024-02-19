d38e321dd3404b113f42803788d5726c921f4ed6
SamuelPerezCO <sernasamuelperez@gmail.com>
2024-02-19 11:50:59 -0500

    Seccion 36

---
+++ Seccion 36/tipo_none.py
@@ -0,0 +1,20 @@
+variable = None
+
+if variable:
+    print('Se considera Verdadera')
+else:
+    print('Se considera falsa')
+
+print(type(variable))
+
+"""
+Ocho formas de generar un bool False en Python
+1. Comilla simple o doble ''/"" (cadena vacia)
+2. Lista Vacia []
+3.Tupla vacia()
+4.Diccionario Vacio {}
+5. Entero 0 
+6.Flotante 0.0
+7. bool False
+8.None
+"""
